e45490e5c155624c60774909c5a4d918b9b77468
Bassem Dimassi <dimassibassem99@gmail.com>
2022-01-31 01:12:30 +0100

    delete value

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 function App() {
 
     const [movieName, setMovieName] = useState('');
     const [review, setReview] = useState('');
+    const [movieReviewList, setMovieReviewList] = useState([])
+    useEffect(() => {
+        axios.get("http://localhost:3001/api/get").then((response) => {
+            setMovieReviewList(response.data)
+        })
+    },[])
 
+    const deleteReview = (movie) => {
+        axios.delete(`http://localhost:3001/api/delete/${movie}`)
+    }
     const submitReview = () => {
         axios.post("http://localhost:3001/api/insert", {
             movieName: movieName, movieReview: review
-        }).then(() => {
-            alert('Successfully added')
-        })
+        });
+
+        setMovieReviewList([...movieReviewList, {movieName: movieName, movieReview: review}]);
+
     }
     return (
         <div className="App">
@@ -31,6 +41,20 @@ function App() {
                     setReview(e.target.value);
                 }}/>
                 <button onClick={submitReview}>Submit</button>
+                {movieReviewList.map((item) => {
+                    return (<div className="card">
+
+                        <h1> MovieName: {item.movieName}</h1>
+                        <p>MovieReview: {item.movieReview}</p>
+                        <button onClick={() => {
+                            deleteReview(item.movieName);
+                        }}>Delete
+                        </button>
+                        <input id='updateInput' type='text'/>
+                        <button>Update</button>
+
+                    </div>)
+                })}
             </div>
         </div>
     );
